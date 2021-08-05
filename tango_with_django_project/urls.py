d8cf026cddba4c5914cf00e880d57159d5441824
Xenios Theodorou <2602538t@student.gla.ac.uk>
2021-08-05 13:31:16 +0300

    User Profile

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -4,6 +4,8 @@ from django.urls import include
 from rango import views
 from django.conf import settings
 from django.conf.urls.static import static
+from registration.backends.simple.views import RegistrationView
+from django.urls import reverse
 """tango_with_django_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -22,11 +24,17 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+class MyRegistrationView(RegistrationView):
+    def get_success_url(self, user):
+        return reverse('rango:register_profile')
+
+
 urlpatterns = [
 path('', views.index, name='index'),
 path('rango/', include('rango.urls')),
 # The above maps any URLs starting with rango/ to be handled by rango. 
 path('admin/', admin.site.urls),
+path('accounts/register/', MyRegistrationView.as_view(), name='registration_register'),
 path('accounts/', include('registration.backends.simple.urls')),
 path('oauth/', include('social_django.urls', namespace='social')), 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
